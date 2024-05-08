671362b5e0b154e09d57e68388df41359b9606c8
Tarun Kumar <tarunkumar@taruns-mbp.home>
2024-05-08 11:36:39 -0400

    add annoation handler

--- src/main/java/com/sample/audit/AuditInterceptor.java
+++ src/main/java/com/sample/audit/AuditInterceptor.java
@@ -5,20 +5,21 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.lang.Nullable;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 public class AuditInterceptor implements HandlerInterceptor {
     private final Logger log = LoggerFactory.getLogger(AuditInterceptor.class);
 
-    private  final AuditService auditService;
+    private final AuditService auditService;
 
     public AuditInterceptor(AuditService auditService) {
         this.auditService = auditService;
     }
 
     @Override
-     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         log.info("pre handle is invoked");
         return true;
     }
@@ -33,7 +34,11 @@ public class AuditInterceptor implements HandlerInterceptor {
 
         log.info("after completions is invoked with request " + request.getMethod() + " and response: " + response.getStatus());
 
-       auditService.startAudit(request.getPathInfo(), String.valueOf((response.getStatus())));
+        if (handler instanceof HandlerMethod) {
+            Audit annotation = ((HandlerMethod) handler).getMethodAnnotation(Audit.class);
+            log.info("annotated with value :  " + annotation.value());
+            auditService.startAudit(request.getPathInfo(), String.valueOf((response.getStatus())));
+        }
     }
 
 }
